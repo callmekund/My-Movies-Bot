ab4eebcaf3ebe7846a8a60689273fd4ff380e1e3
callmekund <gauravbanna5555@gmail.com>
2026-05-14 00:34:10 +0530

    Update button text to 'Watch Now'

--- plugins/channel.py
+++ plugins/channel.py
@@ -370,7 +370,7 @@ async def send_movie_update(bot, base_name):
             text = generate_movie_message(movie_doc, base_name)
             buttons = InlineKeyboardMarkup([[
                 InlineKeyboardButton(
-                    'ɢᴇᴛ ғɪʟᴇs',
+                    '🧩 𝗪𝗮𝘁𝗰𝗵 𝗡𝗼𝘄 🧩',
                     url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}"
                 )
             ]])
@@ -419,7 +419,7 @@ async def update_movie_message(bot, base_name):
         text = generate_movie_message(movie_doc, base_name)
         buttons = InlineKeyboardMarkup([[
             InlineKeyboardButton(
-                'ɢᴇᴛ ғɪʟᴇs',
+                '🧩 𝗪𝗮𝘁𝗰𝗵 𝗡𝗼𝘄 🧩',
                 url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}"
             )
         ]])
