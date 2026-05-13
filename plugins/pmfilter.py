901f2c66bf644d79179e8236387af5247f9f7332
callmekund <gauravbanna5555@gmail.com>
2026-05-14 00:50:06 +0530

    Update button URLs in pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1563,7 +1563,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('ᴅʀᴇᴀᴍxʙᴏᴛᴢ 📜', url='https://github.com/DreamXBotz/Auto_Filter_Bot.git'),
+            InlineKeyboardButton('Gaurav Banna📜', url='https://t.me/call_me_kund'),
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ⇋', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
